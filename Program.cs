d734409d6acb4f3bf4a67f779e3f31839202dbb6
AppyTasty <45888143+AppyTasty@users.noreply.github.com>
2018-12-14 18:11:45 -0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace WindowsFormsApp1
+namespace Sumof2numbers
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Console.Write("Please enter a first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Please enter a second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            int sum = num1 + num2;
+            Console.WriteLine("The sum of two nos is: {0}", sum);
+            Console.ReadLine();
         }
     }
 }
